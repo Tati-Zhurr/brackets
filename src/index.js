ef6eb5e656dbe55b4a683baa1bdd749c7b9d64e7
Tati-Zhurr <zhtv88@gmail.com>
2022-08-04 19:01:12 +0200

    feet index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
   let strNew =str;
  for (let i=0; i<bracketsConfig.length; i++){
-    console.log (strNew);
     if (strNew.includes(bracketsConfig[i].join(''))) {
     strNew=strNew.replace(bracketsConfig[i].join(''),'');
     i=-1;
   };
 };
-console.log (strNew);
 return (strNew === '');
 }
 
-/*console.log (check('([{}])', [['(', ')'], ['[', ']'], ['{', '}']]));*/
