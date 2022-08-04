25b0529f27236a4d016fa87f2a0fa2e32fd9c701
Tati-Zhurr <zhtv88@gmail.com>
2022-08-04 18:39:46 +0200

    feet index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let strNew =str;
+ for (let i=0; i<bracketsConfig.length; i++){
+    console.log (strNew);
+    if (strNew.includes(bracketsConfig[i].join(''))) {
+    strNew=strNew.replace(bracketsConfig[i].join(''),'');
+    i=-1;
+  };
+};
+console.log (strNew);
+return (strNew === '');
 }
+
+/*console.log (check('([{}])', [['(', ')'], ['[', ']'], ['{', '}']]));*/
